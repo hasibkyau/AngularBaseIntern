3275e485741fe55d897f60fd1166383fba4243fc
hasibkyau <hasibkyau.cses@gmail.com>
2024-07-02 11:42:55 +0600

    optimized

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,15 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { BannerComponent } from './banner/banner.component';
-import { CardComponent } from './card/card.component';
-import { ButtonComponent } from './button/button.component';
-import { NewArrivalsComponent } from './new-arrivals/new-arrivals.component';
-import { FooterModule } from 'src/app/shared/components/footer/footer.module';
-import { FooterComponent } from 'src/app/shared/components/footer/footer.component';
 import { HeaderModule } from 'src/app/core/header/header.module';
 import { ProductCardModule } from 'src/app/shared/components/product-card/product-card.module';
-import { AddProductComponent } from './add-product/add-product.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from 'src/app/materials/materials.module';
 
@@ -18,12 +11,6 @@ import { MaterialModule } from 'src/app/materials/materials.module';
 @NgModule({
   declarations: [
     HomeComponent,
-    BannerComponent,
-    CardComponent,
-    ButtonComponent,
-    NewArrivalsComponent,
-    AddProductComponent,
-    // FooterComponent
   ],
   imports: [
     CommonModule,
